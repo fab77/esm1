69bb91f9f168837c2e32136ec5090a1eb09eeb17
Fabrizio Giordano <fabriziogiordano77@gmail.com>
2022-09-19 11:39:03 +0200

    Added Node min version in package.json

--- Box.ts
+++ Box.ts
@@ -1,6 +1,4 @@
 import { BoxObject } from "./BoxObject.js";
-// import fetch from "node-fetch";
-// import { fetch } from "cross-fetch";
 
 export class Box {
 
@@ -23,38 +21,6 @@ export class Box {
         } else {
             return response.arrayBuffer();
         }
-        // if (typeof window !== "undefined") {
-        //     return this.getFileBrowser(uri);
-
-        // } else {
-        //     return this.getFileNode(uri);
-        // }
 
     }
-
-    // async getFileNode(uri: string): Promise<ArrayBuffer> {
-    //     const response = await fetch(uri, {
-    //         method: "GET",
-    //         headers: {
-    //           "Accept": "*",
-    //           "Content-Type": "image/fits",
-    //           "Access-Control-Allow-Origin": "*",
-    //           "Access-Control-Allow-Headers": "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control"
-    //         },
-    //       });
-    //     if (!response.ok) {
-    //         return new ArrayBuffer(0);
-    //     }
-    //     return response.arrayBuffer();
-    // }
-
-    // async getFileBrowser(uri: string): Promise<ArrayBuffer> {
-    //     const response = await window.fetch(uri);
-    //     if (!response.ok) {
-    //         return new ArrayBuffer(0);
-    //     }
-    //     return response.arrayBuffer();
-    // }
-
-
 }
